bd505e1de6975811e19cf29195a3e164885537b9
Ian McCall <ian@ianmccall.codes>
2021-03-20 18:48:36 -0500

    Improve offline support

--- sw.js
+++ sw.js
@@ -1,16 +1,23 @@
 /*jshint esnext: true, browser: true, devel: true, worker: true*/
 /*globals caches*/
 var self = this;
-var CACHENAME = "codes-ianmccall-v3";
+var CACHENAME = "codes-ianmccall-v5";
 var precache = [
+    '/',
+    '/?utm_source=homescreen',
+    '/assets/icons/ianmcodes_logo/maskable_icon_x512.png',
+    '/assets/icons/ianmcodes_logo/maskable_icon_x192.png',
     '/assets/images/banner.jpg',
+    '/css/main-1024.css',
+    '/css/main-1440.css',
+    '/css/main-4k.css',
     '/css/syntax.css',
-    '/css/custom_icons.css',
     '/css/octicons/octicons.css',
     '/css/octicons/octicons.woff',
     '/css/repo_list.css',
     '/js/swinit.js',
-    '/css/home.css'
+    '/js/deferstyles.js',
+    '/assets/images/IanMcCallCodes-logo/vector/default.svg',
 ];
 
 self.addEventListener('install', function(event) {
@@ -41,17 +48,25 @@ self.addEventListener('fetch', function(event) {
     if(url.origin !== location.origin) {
         return;
     }
-    if(url.origin === location.origin && url.pathname.match(/^\/assets\/icons/)) {
-        event.respondWith(handleIcon(event));
-        return;
-    }
+    // if(url.origin === location.origin && url.pathname.match(/^\/assets\/icons/)) {
+    //     event.respondWith(handleIcon(event));
+    //     return;
+    // }
     event.respondWith(
         caches.match(event.request)
         .then(function(response) {
             if(!!response) {
                 return response;
             }
-            return fetch(event.request);
+            return fetch(event.request)
+            .then((response) => {
+                let respClone = response.clone();
+                caches.open(CACHENAME)
+                .then((cache) => {
+                    cache.put(event.request, respClone);
+                });
+                return response;
+            });
         })
     );
 });
@@ -65,6 +80,7 @@ function handleIcon(event) {
             .then(function(cache) {
                 cache.put(event.request,respClone);
             });
+            return response;
         })
     );
     
